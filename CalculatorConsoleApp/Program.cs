65680a62a2055dc4f81130be2ddeba3ae4ec5d04
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-05-30 20:19:32 +0330

    Base project initialized.

--- CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.WriteLine("Calculator Console App");
+﻿
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("-----------------------------------------------------------");
+    Console.WriteLine("Welcome to Calculator Console App");
+    Console.WriteLine("-----------------------------------------------------------");
+
+    Console.WriteLine();
+
+    Console.WriteLine("------------------- Application options -------------------");
+    Console.WriteLine();
+    Console.WriteLine("1. Sum");
+    Console.WriteLine("2. Subtract");
+    Console.WriteLine("3. Multiply");
+    Console.WriteLine("4. Divide");
+    Console.WriteLine("5. Power");
+    Console.WriteLine("6. Square Root");
+    Console.WriteLine("7. Square Root");
+    Console.WriteLine("8. Percent");
+    Console.WriteLine("9. Factorial");
+    Console.WriteLine("10. Exit");
+    Console.WriteLine();
+    Console.WriteLine("-----------------------------------------------------------");
+
+    Console.Write("Please select your option: ");
+
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            break;
+
+        case "2":
+            break;
+
+        case "3":
+            break;
+
+        case "4":
+            break;
+
+        case "5":
+            break;
+
+        case "6":
+            break;
+
+        case "7":
+            break;
+
+        case "8":
+            break;
+
+        case "9":
+            break;
+
+        case "10":
+            break;
+
+        default:
+            break;
+    }
+}
