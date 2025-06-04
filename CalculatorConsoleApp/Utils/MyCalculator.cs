a5774d3ace90b5474b42808fe60485f619d43e6e
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-04 11:55:48 +0330

    MyClaculator class to handle math operations created.

---
+++ CalculatorConsoleApp/Utils/MyCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculatorConsoleApp.Utils
+{
+    public static class MyCalculator
+    {
+        public static int Sum(params string[] numbers)
+        {
+            int sum = 0;
+
+            try
+            {
+                foreach (var number in numbers)
+                {
+                    sum += int.Parse(number);
+                }
+
+                return sum;
+            }
+            catch (FormatException e)
+            {
+                return -1;
+            }
+        }
+    }
+}
